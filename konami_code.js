dcf5b912a4b8671c86e6de7355a87613e88cedf3
Sam Power <sam.g.power@gmail.com>
2019-03-18 19:38:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init() {
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', e => onKeyDownHandler(e))
 }
+ 
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key)
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+      
+      

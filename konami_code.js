7f88124db83cbb7998cb19b2a440df1159f2ebe5
Sam Power <sam.g.power@gmail.com>
2019-03-18 19:33:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38]
+
+// 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', e => console.log(e.which))
 }
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+      
+      
